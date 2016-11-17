6b52df22c86c6b7796ca1b43c103cf22efc4ccca
Ben Schmeckpeper <ben.schmeckpeper@gmail.com>
2016-11-16 19:45:35 -0600

    Add name column to User

--- web/models/user.ex
+++ web/models/user.ex
@@ -1,18 +1,23 @@
 defmodule ExGnarl.User do
   use ExGnarl.Web, :model
+  use Ecto.Schema
+  import Ecto.Changeset
+
+  alias Ueberauth.Auth
 
   schema "users" do
     field :email, :string
+    field :name, :string
 
     timestamps()
   end
 
   @doc """
-  Builds a changeset based on the `struct` and `params`.
+  Builds a changeset based on the `user` and `params`.
   """
-  def changeset(struct, params \\ %{}) do
-    struct
-    |> cast(params, [:email])
-    |> validate_required([:email])
+  def changeset(user, params \\ %{}) do
+    user
+    |> cast(params, [:email, :name])
+    |> validate_required([:email, :name])
   end
 end
