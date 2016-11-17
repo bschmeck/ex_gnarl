d117dd58432a9e6e0ec7fe41e459ce6973dbc064
Ben Schmeckpeper <ben.schmeckpeper@gmail.com>
2016-11-17 10:42:05 -0600

    Find or create a User after Google auth

--- web/controllers/auth_controller.ex
+++ web/controllers/auth_controller.ex
@@ -1,4 +1,37 @@
 defmodule ExGnarl.AuthController do
+  require Logger
+
   use ExGnarl.Web, :controller
   plug Ueberauth
+
+  alias ExGnarl.User, as: User
+
+  def callback(%{assigns: %{ueberauth_auth: auth}} = conn, _params) do
+    case find_or_create(auth.provider, auth.info) do
+      {:ok, user} ->
+        conn
+        |> put_flash(:info, "Successfully logged in.")
+        |> put_session(:current_user, user)
+        |> redirect(to: "/")
+      {:error, reason} ->
+        conn
+        |> put_flash(:error, reason)
+        |> redirect(to: "/")
+    end
+  end
+
+  defp find_or_create(:google, info) do
+    %{email: email} = info
+
+    query = from u in User, where: u.email == ^email
+    user = case Repo.one(query) do
+             %User{} = user -> user
+             _ ->
+               %User{}
+               |> User.changeset(Map.from_struct(info))
+               |> Repo.insert!
+           end
+
+    {:ok, user}
+  end
 end
